73bb4f9e240d526b6f15593f4ee28b57d6144c4e
Michaël Dohr <dohr.michael@gmail.com>
2023-12-04 15:19:59 +0100

    D4 (#1)
    * Create d4.txt
    
    * Create D4.scala
    
    * Create DX.scala
    
    * Create D4Spec.scala
    
    * Create DXSpec.scala

---
+++ src/test/scala/io/dohrm/adventOfCode/y2023/DXSpec.scala
@@ -0,0 +1,14 @@
+package io.dohrm.adventOfCode.y2023
+
+class DXSpec extends munit.FunSuite {
+  val sample: String =
+    """""".stripMargin
+
+  test("part1") {
+    assertEquals(DX.part1(sample), 0)
+  }
+
+  test("part2") {
+    assertEquals(DX.part2(sample), 0)
+  }
+}
