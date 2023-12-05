120b7bf07779981477453d2b07a50e57dcb7f9ef
Michaël Dohr <dohr.michael@gmail.com>
2023-12-05 19:45:56 +0100

    Create D5Spec.scala

---
+++ src/test/scala/io/dohrm/adventOfCode/y2023/D5Spec.scala
@@ -0,0 +1,46 @@
+package io.dohrm.adventOfCode.y2023
+
+class D5Spec extends munit.FunSuite {
+  val sample: String =
+    """seeds: 79 14 55 13
+      |
+      |seed-to-soil map:
+      |50 98 2
+      |52 50 48
+      |
+      |soil-to-fertilizer map:
+      |0 15 37
+      |37 52 2
+      |39 0 15
+      |
+      |fertilizer-to-water map:
+      |49 53 8
+      |0 11 42
+      |42 0 7
+      |57 7 4
+      |
+      |water-to-light map:
+      |88 18 7
+      |18 25 70
+      |
+      |light-to-temperature map:
+      |45 77 23
+      |81 45 19
+      |68 64 13
+      |
+      |temperature-to-humidity map:
+      |0 69 1
+      |1 0 69
+      |
+      |humidity-to-location map:
+      |60 56 37
+      |56 93 4""".stripMargin
+
+  test("part1") {
+    assertEquals(D5.part1(sample), 35L)
+  }
+
+  test("part2") {
+    assertEquals(D5.part2(sample), 1L)
+  }
+}
