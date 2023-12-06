2698395e870593585b90718b169fec3c80cff78f
Michael DOHR <dohr.michael@gmail.com>
2023-12-06 14:10:05 +0100

    feat: d6

--- src/main/scala/io/dohrm/adventOfCode/y2023/D6.scala
+++ src/main/scala/io/dohrm/adventOfCode/y2023/D6.scala
@@ -4,17 +4,24 @@ import scala.collection.mutable
 
 object D6 extends Base {
   case class Race(time: Long, distance: Long) {
-    def wayToWin: List[Long] = {
-      var inc = 1L
-      val res = mutable.Buffer[Long]()
-      while (inc <= time) {
-        val c = inc * (time - inc)
-        if (c > distance) {
-          res.append(inc)
-        }
-        inc += 1
-      }
-      res.toList
+    // ax² + bx + c = 0
+    // x1 = (-b + (b²-4ac)^1/2)/2a
+    // x2 = (-b - (b²-4ac)^1/2)/2a
+
+    // -x² + time * x - distance > 0
+    // a = -1
+    // b = time
+    // c = -distance
+    def countWayToWin: Long = {
+      val base = Math.pow(Math.pow(time.toDouble, 2) - 4d * -1d * -distance.toDouble, 0.5)
+      val x1 = (-time.toDouble + base) / 2d * -1d
+      val x2 = (-time.toDouble - base) / 2d * -1d
+      val min = Math.min(x1, x2)
+      val max = Math.max(x1, x2)
+
+      // Exclusive
+      val maxB = if (max.toLong.toDouble == max) max.toLong - 1 else max.toLong
+      maxB - min.toLong
     }
   }
 
@@ -27,14 +34,15 @@ object D6 extends Base {
 
   def part1(value: String): Long = {
     val races = parse(value)
-    races.map(_.wayToWin.length).product
+
+    races.map(_.countWayToWin).product
   }
 
   def part2(value: String): Long = {
     val races = parse(value)
     val tmp = races.foldLeft(("", "")) { (acc, c) => (acc._1 + c.time.toString, acc._2 + c.distance.toString) }
     val race = Race(tmp._1.toLong, tmp._2.toLong)
-    race.wayToWin.length
+    race.countWayToWin
   }
 
   def main(args: Array[String]): Unit = {
